ea95b4448de15bf25e185712da48634b57a44b52
MORI Kazutaka <mori.kazutaka@gmail.com>
2025-07-22 14:50:13 +0900

    作業用ディレクトリ /workspace を作成

--- my_scripts/setup_sshd.sh
+++ my_scripts/setup_sshd.sh
@@ -15,3 +15,7 @@ mkdir /var/run/sshd
 # 公開鍵を置くディレクトリをユーザー rstudio で作っておく
 mkdir /home/rstudio/.ssh/
 chown -R rstudio:rstudio /home/rstudio/.ssh/
+
+# 作業用ディレクトリ
+mkdir /workspace
+chmod 777 /workspace
